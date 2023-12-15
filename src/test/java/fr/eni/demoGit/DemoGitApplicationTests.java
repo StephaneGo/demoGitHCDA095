70a0571edfb7307d5dd3abde3c46fed45216b0d0
Stephane G <sgobin@campus-eni.fr>
2023-12-15 16:45:27 +0100

    version initiale du projet

---
+++ src/test/java/fr/eni/demoGit/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.demoGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
